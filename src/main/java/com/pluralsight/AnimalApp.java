43295874c18e1ea8f1243bd732e2fcdf70a3580f
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-27 11:58:46 -0400

    animal inheritance

---
+++ src/main/java/com/pluralsight/AnimalApp.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class AnimalApp {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.breathe();
+        dog.sniffButt();
+    }
+}
---
+++ src/main/java/com/pluralsight/Dog.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class Dog extends Animal {
+
+    public void sniffButt(){
+        System.out.println("Place head in read of other dog. Sniff, Sniff, Sniff");
+    }
+}
